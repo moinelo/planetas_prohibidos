ac08313b0b0b4c45b09d3398b25ae4a35545ac68
moinelo <57208112+moinelo@users.noreply.github.com>
2026-09-25 12:08:13 +0200

    Update image size in comments subscription link

--- ultimoscomentarios.js
+++ ultimoscomentarios.js
@@ -64,5 +64,5 @@ function showrecentcomments(json)
       document.write('<br/><br/>');
     }
   }
-  document.write('<div style="font-size:78%;text-align:center"><img src="https://moinelo.github.io/planetas_prohibidos/rss.png" width="24" height="24" /><a href="http://planetasprohibidos.blogspot.com/feeds/comments/default"> Suscribete a los comentarios (Atom)</a></div>');
+  document.write('<div style="font-size:78%;text-align:center"><img src="https://moinelo.github.io/planetas_prohibidos/rss.png" width="16" height="16" /><a href="http://planetasprohibidos.blogspot.com/feeds/comments/default"> Suscribete a los comentarios (Atom)</a></div>');
                  }
